06c11067de5924f184e15a395804dbd8259c429c
Michael <clonex_kontakt@hotmail.com>
2021-12-19 19:16:35 +0100

    Updated SolversPage

--- src/components/SolversPage.js
+++ src/components/SolversPage.js
@@ -1,16 +1,11 @@
 import React from "react";
 
-import {API} from "../helpers.js";
+import {API, getInputValues} from "../helpers.js";
 
 export default class SolversPage extends React.Component {
     state = {
-        solvers: [{
-            id: 10,
-            name: "test",
-
-        }],
-
-        editing: 0,
+        solvers: [],
+        editing: false,
     };
 
     componentDidMount()
@@ -20,11 +15,11 @@ export default class SolversPage extends React.Component {
 
     async getData()
     {
-        const data = await API.call("GET", "/solvers");
+        const data = await API.call("GET", "solvers");
         if(data && !data.error)
         {
             this.setState({
-                data: data.data,
+                solvers: data.data,
             });
         }
     }
@@ -34,23 +29,25 @@ export default class SolversPage extends React.Component {
         const check = window.confirm("Are you sure you want to delete it?");
         if(check)
         {
-            await API.call("DELETE", "/solvers/" + id);
+            await API.call("DELETE", "solvers/" + id);
             await this.getData();
         }
     }
 
-    async upload(e)
+    async save(e)
     {
-        const file = e.target.files[0];
-        let data = new FormData();
-        data.append('file', file);
-
-        const resp = await API.call("PUT", "/solvers/" + this.state.editing + "/image", data, {
-            "Content-Type": "application/x-www-form-urlencoded",
-        });
+        e.preventDefault();
+        const data = getInputValues(e.target.elements);
+        const isEditing = typeof this.state.editing === "number";
+        const resp = await (isEditing ? API.call("PUT", "solvers/" + this.state.editing, data) : API.call("POST", "solvers", data));
         if(resp && !resp.error)
         {
-            alert("The file has been uploaded!");
+            this.setState({
+                editing: false,
+            });
+            this.getData();
+        }else{
+            alert("A unkown error happened, try again");
         }
     }
 
@@ -64,21 +61,23 @@ export default class SolversPage extends React.Component {
             {
                 (isEditing || isNew) && <div className="pb-2 mb-2 border-bottom">
                     <h1>{isEditing ? "Editing solver" : "New solver"}</h1>
-                    <div className="row">
-                        <div className="col-sm-2">
-                            <label htmlFor="name">Name</label>
+                    <form onSubmit={e => this.save(e)}>
+                        <div className="row">
+                            <div className="col-sm-2">
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <div className="col-sm-10 pb-1">
+                                <input className="form-control" name="name" required={true} defaultValue={editingData?.name} />
+                            </div>
+                            <div className="col-sm-2">
+                                <label htmlFor="name">Image</label>
+                            </div>
+                            <div className="col-sm-10">
+                                <input className="form-control" name="docker_image" required={true} defaultValue={editingData?.docker_image} />
+                            </div>
                         </div>
-                        <div className="col-sm-10 pb-1">
-                            <input className="form-control" name="name" defaultValue={editingData.name} />
-                        </div>
-                        <div className="col-sm-2">
-                            <label htmlFor="name">Image</label>
-                        </div>
-                        <div className="col-sm-10">
-                            <input className="form-control" type="file" name="name" disabled={isNew} accept=".png,.jpg" onChange={(e) => this.upload(e)} />
-                        </div>
-                    </div>
-                    <button onClick={() => this.setState({editing: "new"})} className="btn btn-light">Save</button>
+                        <button className="btn btn-light">Save</button>
+                    </form>
                 </div>
             }
             <h1>Solvers</h1>
@@ -88,7 +87,7 @@ export default class SolversPage extends React.Component {
                     <tr>
                     <th scope="col">#</th>
                     <th scope="col">Name</th>
-                    <th scope="col"></th>
+                    <th scope="col">Image</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -96,6 +95,7 @@ export default class SolversPage extends React.Component {
                         this.state.solvers.map((data, key) => <tr key={data.id}>
                             <th scope="row">{data.id}</th>
                             <td>{data.name}</td>
+                            <td>{data.docker_image}</td>
                             <td>
                                 <button onClick={() => this.setState({editing: key})} className="btn m-1 mt-0 mb-0 btn-secondary">Edit</button>
                                 <button onClick={() => this.delete(data.id)} className="btn btn-danger">Delete</button>
