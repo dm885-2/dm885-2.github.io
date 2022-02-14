744b98c9c54d91568b17bf055aad95dfc16487fe
Kaaasper <43987931+Kaaasper@users.noreply.github.com>
2022-02-14 02:53:23 +0100

    url update

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-export const IP = process.env?.apiURL ?? ((!process.env.NODE_ENV || process.env.NODE_ENV === 'development') && 0 ? "http://localhost:8080" : "https://iamdomain.tigris.club/"); // Gateway IP
+export const IP = process.env?.apiURL ?? ((!process.env.NODE_ENV || process.env.NODE_ENV === 'development') && 0 ? "http://localhost:8080" : "https://dm885api.tigris.club/"); // Gateway IP
 
 export function getInputValues(elements)
 {
